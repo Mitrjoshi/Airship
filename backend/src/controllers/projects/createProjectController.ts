eec913bdfa36c88e660c2ee16818172b1e94c6f9
koushik bharati <koushikbharati7@gmail.com>
2024-11-04 20:05:44 +0530

    merge

--- backend/src/controllers/projects/createProjectController.ts
+++ backend/src/controllers/projects/createProjectController.ts
@@ -1,5 +1,6 @@
 import { CreateProjectRequest } from "@/interface/request";
-import { CreateProject } from "@/services/projectServices";
+import { createS3Bucket } from "@/services/AWSServices";
+import { createProject } from "@/services/projectServices";
 import { createResponse } from "@/utils/createResponse";
 import { Request, Response } from "express";
 
@@ -8,7 +9,12 @@ export const createProjectController = async (
   res: Response
 ) => {
   try {
-    const data = await CreateProject(req.body);
+    const data = await createProject(req.body);
+    const bucketName = `project-${data.name
+      .toLowerCase()
+      .replace(/\s+/g, "-")}`;
+
+    await createS3Bucket(bucketName, req.body.workspace_id);
 
     const projectData = {
       projectId: data.id,
