9a108a6a7cd030e3932f655a007ee7d723817b5a
Mitr Joshi <mitrjoshi26@gmail.com>
2024-11-29 18:01:16 +0530

    1

--- frontend/src/routes/ProjectPage.tsx
+++ frontend/src/routes/ProjectPage.tsx
@@ -2,14 +2,13 @@ import PageContainer from '@/components/shared/PageContainer'
 import TitleHeader from '@/components/shared/TitleHeader'
 import { Button } from '@/components/ui/button'
 import { useGetSingleProject } from '@/services/useGetSingleProject'
-import { Outlet, useNavigate, useParams } from 'react-router-dom'
-import { FolderOpen, SettingsIcon } from 'lucide-react'
+import { Link, Outlet, useParams } from 'react-router-dom'
+import { FolderOpen, SettingsIcon, Users } from 'lucide-react'
 import { PlusIcon, UpdateIcon } from '@radix-ui/react-icons'
 import LoadingButton from '@/components/shared/LoadingButton'
 
 export default function ProjectPage() {
   const { projectId } = useParams()
-  const navigate = useNavigate()
 
   const { refetch: refetchProjectData, isRefetching: isProjectDataLoading } = useGetSingleProject(projectId as string)
 
@@ -30,20 +29,31 @@ export default function ProjectPage() {
               Refresh
             </LoadingButton>
 
-            <Button onClick={() => navigate('source')} variant='outline' size='sm'>
-              <FolderOpen className='h-4 w-4' />
-              Source
+            <Link to={'source'}>
+              <Button variant='outline' size='sm'>
+                <FolderOpen className='h-4 w-4' />
+                Source
+              </Button>
+            </Link>
+
+            <Link to={'config'}>
+              <Button variant='outline' size='sm'>
+                <SettingsIcon className='h-4 w-4' />
+                Configurations
+              </Button>
+            </Link>
+
+            <Button variant='outline' size='sm'>
+              <Users className='h-4 w-4' />
+              Members
             </Button>
 
-            <Button onClick={() => navigate('config')} variant='outline' size='sm'>
-              <SettingsIcon className='h-4 w-4' />
-              Configurations
-            </Button>
-
-            <Button onClick={() => navigate('upload')} size='sm'>
-              <PlusIcon className='h-4 w-4' />
-              New Deployment
-            </Button>
+            <Link to={'upload'}>
+              <Button size='sm'>
+                <PlusIcon className='h-4 w-4' />
+                New Deployment
+              </Button>
+            </Link>
           </div>
         }
       />
